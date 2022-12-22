808b56f1444f3025e397390d22efb3b80180414a
Baruf Kosgei <barufkosgei@gmail.com>
2022-12-22 13:50:26 -0600

    build: create profile page

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,2 +1,8 @@
-const Profile = () => {};
+import FormRow from '../../components/FormRow';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useDispatch, useSelector } from 'react-redux';
+const Profile = () => {
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+};
 export default Profile;
